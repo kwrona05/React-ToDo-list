70360675b717242c10a647a28c31637d4d1e212e
kwrona05 <kwrona05@gmail.com>
2024-08-06 11:00:40 +0200

    Create title component

---
+++ Reac-test-project/src/Tititle.jsx
@@ -0,0 +1,9 @@
+function Name() {
+    return (
+        <div>
+    <h1>ToDo list application</h1>
+    <p1>This app is created to practice react and improve my skills about components </p1>
+    </div>
+    )
+}
+export default Name
